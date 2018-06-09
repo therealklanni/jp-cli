f203ac332ba87d711ab3b37e5b162ce7e35e558a
Kevin Lanni <therealklanni@gmail.com>
2018-06-09 15:10:59 -0700

    Basic read implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::prelude::*;
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate serde_json;
+use serde_json::{Value, from_str};
+
 fn main() {
-    println!("Hello, world!");
+  let matches = clap_app!(jp =>
+    (version: "0.1.0")
+    (author: "Kevin Lanni <therealklanni@gmail.com>")
+    (about: "Simple JSON parser/inspector")
+    (@arg FILE: -f --file +takes_value "JSON file to parse")
+    (@arg PATTERN: "Query pattern")
+  ).get_matches();
+
+  let filename = matches.value_of("FILE").unwrap();
+  let pattern = matches.value_of("PATTERN").unwrap_or("");
+  let mut f = File::open(filename).expect("file not found");
+  let mut contents = String::new();
+
+  f.read_to_string(&mut contents).expect("error reading file");
+
+  let value: Value = from_str(&contents).unwrap();
+
+  let prefix: String = "/".to_owned() + pattern;
+
+  let pointer: &str = &prefix.replace(".", "/");
+
+  println!("{:#}", contents);
+  println!("{:?}", pointer);
+  println!("{:#}", value.pointer(pointer).unwrap_or(&json!("")));
 }
