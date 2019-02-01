45c2f7fc0e5fdfee05f2bb4683676d6fd22b4c19
Kevin Lanni <therealklanni@gmail.com>
2019-01-31 23:33:24 -0700

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::process::exit;
 #[macro_use]
 extern crate clap;
 #[macro_use]
@@ -21,34 +22,54 @@ fn main() {
 
     if matches.is_present("FILE") {
         let filename = matches.value_of("FILE").unwrap();
-        let mut contents = String::new();
-        let mut file = File::open(filename).expect("file not found");
+        let mut file = match File::open(filename) {
+            Ok(file) => file,
+            Err(e) => {
+                println!("Error: {}: {}", e, filename);
+                exit(1);
+            }
+        };
 
-        file.read_to_string(&mut contents)
-            .expect("I/O error reading file");
+        let mut contents = String::new();
+        let size = match file.read_to_string(&mut contents) {
+            Ok(size) => size,
+            Err(e) => {
+                println!("Error: {}", e);
+                exit(2);
+            }
+        };
+
+        if size == 0 {
+            exit(0);
+        }
 
         let value: Value = from_str(&contents).unwrap();
 
         if matches.is_present("PATTERN") {
-            // println!("{:?}", pointer);
             println!("{:#}", value.pointer(pointer).unwrap_or(&json!("")));
         } else {
             println!("{:#}", value);
         }
     } else {
-        println!("No file option specified");
-        let mut buffer = String::new();
         let stdin = io::stdin();
         let mut handle = stdin.lock();
+        let mut buffer = String::new();
 
-        handle
-            .read_to_string(&mut buffer)
-            .expect("I/O error reading buffer");
+        let size = match handle.read_to_string(&mut buffer) {
+            Ok(size) => size,
+            Err(e) => {
+                println!("Error: {}", e);
+                exit(3);
+            }
+        };
+
+        if size == 0 {
+            exit(0);
+        }
 
         let value: Value = from_str(&buffer).unwrap();
 
         if matches.is_present("PATTERN") {
-            // println!("{:?}", pointer);
             println!("{:#}", value.pointer(pointer).unwrap_or(&json!("")));
         } else {
             println!("{:#}", value);
