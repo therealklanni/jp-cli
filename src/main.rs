9654b9090260c8bd134c62dbb347328716052829
Kevin Lanni <therealklanni@gmail.com>
2019-02-08 23:50:44 -0700

    Refactor duplicated code
    - Move duplicated code to functions
    - Improve error handling
    - Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,74 @@
 use std::fs::File;
 use std::io::{self, Read};
 use std::process::exit;
+
 #[macro_use]
 extern crate clap;
-#[macro_use]
+
 extern crate serde_json;
 use serde_json::{from_str, Value};
+
+fn read_from_source<T: Read>(reader: &mut T) -> Value {
+    let mut contents = String::new();
+    let size = match reader.read_to_string(&mut contents) {
+        Ok(size) => size,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            exit(2);
+        }
+    };
+
+    if size == 0 {
+        exit(0);
+    }
+
+    match from_str(&contents) {
+        Ok(json) => json,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            exit(3);
+        }
+    }
+}
+
+fn print_json(value: Value, pointer: String) {
+    match value.pointer(&pointer) {
+        None => eprintln!("Invalid query: {}", pointer[1..].replace('/', ".")),
+        value => println!("{:#}", value.unwrap()),
+    }
+}
 
 fn main() {
     let matches = clap_app!(jp =>
         (version: crate_version!())
-        (about: "JSON Probe (http://github.com/therealklanni/jp)")
+        (about: "JSON Probe (http://github.com/therealklanni/jp-cli)")
         (@arg FILE: -f --file +takes_value "JSON file to probe")
         (@arg PATTERN: "Query pattern")
     )
     .get_matches();
 
     let pattern = matches.value_of("PATTERN").unwrap_or("");
-    let prefixed: String = format!("{}{}", "/", pattern);
-    let pointer: &str = &prefixed.replace(".", "/");
+    let prefixed = format!("{}{}", "/", pattern);
+    let pointer = prefixed.replace(".", "/");
+    let value: Value;
 
     if matches.is_present("FILE") {
         let filename = matches.value_of("FILE").unwrap();
         let mut file = match File::open(filename) {
             Ok(file) => file,
             Err(e) => {
-                println!("Error: {}: {}", e, filename);
+                eprintln!("Error: {}: {}", e, filename);
                 exit(1);
             }
         };
 
-        let mut contents = String::new();
-        let size = match file.read_to_string(&mut contents) {
-            Ok(size) => size,
-            Err(e) => {
-                println!("Error: {}", e);
-                exit(2);
-            }
-        };
-
-        if size == 0 {
-            exit(0);
-        }
-
-        let value: Value = from_str(&contents).unwrap();
-
-        if matches.is_present("PATTERN") {
-            println!("{:#}", value.pointer(pointer).unwrap_or(&json!("")));
-        } else {
-            println!("{:#}", value);
-        }
+        value = read_from_source(&mut file);
     } else {
         let stdin = io::stdin();
         let mut handle = stdin.lock();
-        let mut buffer = String::new();
 
-        let size = match handle.read_to_string(&mut buffer) {
-            Ok(size) => size,
-            Err(e) => {
-                println!("Error: {}", e);
-                exit(3);
-            }
-        };
-
-        if size == 0 {
-            exit(0);
-        }
-
-        let value: Value = from_str(&buffer).unwrap();
-
-        if matches.is_present("PATTERN") {
-            println!("{:#}", value.pointer(pointer).unwrap_or(&json!("")));
-        } else {
-            println!("{:#}", value);
-        }
+        value = read_from_source(&mut handle);
     }
+
+    print_json(value, pointer);
 }
